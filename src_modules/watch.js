db183689305efb68f14763111daeb64d63813742
yeliex <yeliex@yeliex.com>
2016-05-03 11:09:56 +0800

    update Watch to module;

---
+++ src_modules/watch.js
@@ -0,0 +1,32 @@
+/**
+ * © Alexander Buzin, 2014-2015
+ * Site: http://alexbuzin.me/
+ * Email: alexbuzin88@gmail.com
+ */
+
+const Watch = function (queue) {
+
+    'use strict';
+    queue = Array.isArray(queue) ? queue.slice() : [];
+    const add = function (element) {
+
+        queue.push(element);
+
+        return this;
+    };
+    const remove = function (element) {
+
+        queue = queue.filter(function (item) {
+            return item != element;
+        });
+
+        return this;
+    };
+
+    return {
+        add,
+        remove
+    };
+};
+
+module.exports = Watch;
